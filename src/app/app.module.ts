7ce6d27fac278d05d25b31a5a994591a463ad489
Rodrigo Cervone <rodrigo.cervone@hulu.com>
2017-04-27 00:25:52 -0300

    Adds angular-material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -11,8 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    LeomarMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/entry.live.js
+++ src/entry.live.js
@@ -1,10 +1,10 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
 function createWindow () {
   setTimeout(() => {
@@ -16,12 +16,12 @@ function createWindow () {
       pathname: 'localhost:4200',
       protocol: 'http:',
       slashes: true
-    }))
+    }));
 
     // Open the DevTools when in dev mode.
     if(process.env.NODE_ENV=='development') {
-      win.webContents.openDevTools()
-      require('devtron').install()
+      win.webContents.openDevTools();
+      require('devtron').install();
     }
 
     // Emitted when the window is closed.
@@ -29,22 +29,22 @@ function createWindow () {
       // Dereference the window object, usually you would store windows
       // in an array if your app supports multi windows, this is the time
       // when you should delete the corresponding element.
-      win = null
+      win = null;
     })
-  }, 12000)
+  }, 12000);
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
@@ -52,9 +52,9 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
